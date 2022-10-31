8af414e6d6087630ffebe60ec5059c5714d951f8
Franco Dev <gfrancojf@gmail.com>
2022-10-30 20:20:58 -0400

    proyecto base vue

---
+++ resources/js/vue/components/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+
+        <h1> home page 2</h1>
+
+    </div>
+</template>
---
+++ resources/js/vue/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+
+import App from "../vue/components/App.vue"
+
+const app = createApp(App)
+
+app.mount("#app")
+
